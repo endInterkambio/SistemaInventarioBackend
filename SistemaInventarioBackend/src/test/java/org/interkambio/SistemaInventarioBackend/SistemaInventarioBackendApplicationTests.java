4c2ae801fdfc0eaec643f08469eccce6b77d5942
Enmanuel <end199615@gmail.com>
2025-08-14 16:10:16 -0500

    Config for deploy and docker container

--- SistemaInventarioBackend/src/test/java/org/interkambio/SistemaInventarioBackend/SistemaInventarioBackendApplicationTests.java
+++ SistemaInventarioBackend/src/test/java/org/interkambio/SistemaInventarioBackend/SistemaInventarioBackendApplicationTests.java
@@ -2,12 +2,11 @@ package org.interkambio.SistemaInventarioBackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class SistemaInventarioBackendApplicationTests {
-
 	@Test
-	void contextLoads() {
-	}
-
+	void contextLoads() {}
 }
